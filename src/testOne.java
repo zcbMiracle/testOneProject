d5ed9afe92168295d29ee9573ce26575c1dcfaf0
zcbMiracle <913399341@qq.com>
2020-03-31 23:32:58 +0800

    commit test

--- src/testOne.java
+++ src/testOne.java
@@ -5,8 +5,5 @@ public class testOne {
     public static void main(String[] args) {
         System.out.println(123123);
         System.out.println(111222);
-        System.out.println(234234234);
-        System.out.println(34343);
-        System.out.println(555555);
     }
 }
