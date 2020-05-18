f8e4f9afd85839a2bdd352a94e76b2a9385e6756
zcbMiracle <913399341@qq.com>
2020-05-18 23:09:32 +0800

    playDataTest

--- src/testOne.java
+++ src/testOne.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by 91339 on 2019/8/4.
  */
@@ -5,5 +9,21 @@ public class testOne {
     public static void main(String[] args) {
         System.out.println(123123);
         System.out.println(111222);
+        new testOne().removeData();
+    }
+    public void removeData(){
+        List<Integer> integers = new LinkedList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
+        for (Integer i:
+             integers) {
+            if (i==2){
+                integers.set(i,null);
+                integers.remove(i);
+            }
+            System.out.println(integers.size());
+        }
     }
 }
